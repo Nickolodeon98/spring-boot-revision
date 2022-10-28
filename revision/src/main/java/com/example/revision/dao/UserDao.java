3378780e74ef0e2b544fe2ea63ff921ffc5052a4
Nickolodeon98 <sjeon0730@gmail.com>
2022-10-28 13:39:31 +0900

    ADD UserDao 클래스 추가

--- revision/src/main/java/com/example/revision/dao/UserDao.java
+++ revision/src/main/java/com/example/revision/dao/UserDao.java
@@ -1,18 +1,22 @@
 package com.example.revision.dao;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
 
+@Component
 public class UserDao {
 
-    private DataSource dataSource;
-    private JdbcTemplate jdbcTemplate;
+    private final DataSource dataSource;
+    private final JdbcTemplate jdbcTemplate;
 
     public UserDao(DataSource dataSource) {
         this.dataSource = dataSource;
         this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
-
+    public void deleteAll() {
+        this.jdbcTemplate.update("DELETE FROM users");
+    }
 }
